63953a0ce4b72939989fd8813267b55c62ebf5ab
David Akoshile <david.akoshile@yahoo.com>
2023-01-04 09:31:57 -0500

    add

---
+++ 1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -0,0 +1,14 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+let name = "Akoshile David."
+let greeting = "Welcome back home "
+let welcomeMesssage = greeting + name
+
+// Render the welcome message using welcomeEl.innerText
+
+welcomeEl.innerText = welcomeMesssage
---
+++ 1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -0,0 +1,16 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let entries = count + " -"// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+   saveEl.innerText += entries // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+}
