596468e077b6c08dcb9dd250169ac6ca279fd5c1
David Akoshile <david.akoshile@yahoo.com>
2023-01-18 12:01:07 -0500

    Did Some more Class Work

--- 3. Build a Blackjack game/22. Array indexes/index.js
+++ 3. Build a Blackjack game/22. Array indexes/index.js
@@ -1,15 +1,21 @@
 // Arrays - ordered lists of items
 
+// 0 indexed
 let featuredPosts = [
-    "Check out my Netflix clone",
-    "Here's the code for my project",
-    "I've just relaunched my portfolio"
-]
+  "Check out my Netflix clone", // 0
+  "Here's the code for my project", // 1
+  "I've just relaunched my portfolio", // 2
+];
 
+// console.log( featuredPosts[2] )
 
-let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+let experience = [
+  "CEO at Scrimba",
+  "Frontend developer at Xeneta",
+  "People counter for Norstat",
+];
 
 // Make the following appear in the console:
-// Frontend developer at Xeneta
-// People counter for Norstat
-// CEO at Scrimba
+console.log(experience[1]); // Frontend developer at Xeneta
+console.log(experience[2]); // People counter for Norstat
+console.log(experience[0]); // CEO at Scrimba
