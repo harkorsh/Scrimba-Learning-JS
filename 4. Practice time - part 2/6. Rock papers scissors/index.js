1c61e80acf5acccb0e67c2c40b0dc306e2f6a039
David Akoshile <david.akoshile@yahoo.com>
2023-01-24 11:50:37 -0500

    Did Some more updates to my assignments
    This is practice time, I am improving on my Looping and methods

--- 4. Practice time - part 2/6. Rock papers scissors/index.js
+++ 4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -1,5 +1,8 @@
-let hands = ["rock", "paper", "scissor"]
+let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
-
-
+let randomHand = Math.floor(Math.random() * 3);
+function getRandomHand() {
+  return hands[randomHand];
+}
+console.log(getRandomHand(hands));
