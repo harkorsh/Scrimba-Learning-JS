9d3f12d74990a91236fd8a66ffa3d4b3d8b2ae94
David Akoshile <david.akoshile@yahoo.com>
2023-01-26 09:02:01 -0500

    Did some updates

--- 5. Build a Chrome Extension/8. Create the myLeads array and inputEl/index.js
+++ 5. Build a Chrome Extension/8. Create the myLeads array and inputEl/index.js
@@ -1,11 +1,11 @@
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
+let inputEl = document.getElementById("input-el");
+let myLeads = [];
 
-let inputBtn = document.getElementById("input-btn")
+let inputBtn = document.getElementById("input-btn");
 
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
-})
-
-
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked!");
+});
