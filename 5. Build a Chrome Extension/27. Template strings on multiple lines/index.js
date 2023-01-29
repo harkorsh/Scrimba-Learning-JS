24196f1208985dc79f8255b65c29e958f6f249a3
David Akoshile <david.akoshile@yahoo.com>
2023-01-29 14:47:49 -0500

    Continued on my Learning Process

--- 5. Build a Chrome Extension/27. Template strings on multiple lines/index.js
+++ 5. Build a Chrome Extension/27. Template strings on multiple lines/index.js
@@ -1,9 +1,11 @@
 // template strings/literals
 
-const recipient = "James"
-const sender = "Per Harald Borgen"
+const recipient = "James";
+const sender = "David";
 
 // Break the email string into multiple lines
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+const email = `Hey ${recipient}!
+ How is it going? Cheers 
+ ${sender}`;
 
-console.log(email)
+console.log(email);
